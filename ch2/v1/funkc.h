463a39d24f4d167ef652505d31f0c312c855cc41
Misquic <kamilsocko23@gmail.com>
2024-10-20 15:20:25 +0200

    added .gitignore, modified v1/func so its placeholder, added results folders to make empty results folders

--- ch2/v1/funkc.h
+++ ch2/v1/funkc.h
@@ -5,21 +5,6 @@
 #include <string>
 #include <algorithm>
 
-int calc_potential();
-int calc_electric_field();
-int calc_charge_density();
-
-int update_particles_velocity();
-int update_particles_positions();
-
-int load_particles();
-
-int perform_collisions();
-int inject_particles();
-int output_diagnostics();
-
-int output_results();
-
-
+//placeholder file
 
 #endif
